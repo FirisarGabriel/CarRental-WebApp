3621ec9d9fec1d744ee8cbf0c109e83d3a2f7712
FirisarGabriel <gabrielfirisar@gmail.com>
2025-02-22 21:57:58 +0200

    changes

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Proiect_Rent_A_Car.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
